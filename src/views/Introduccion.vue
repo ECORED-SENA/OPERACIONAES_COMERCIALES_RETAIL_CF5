0678cab3323113360f0d6c5720cbe8bdeaef38e3
elrilacho <80546125+elrilacho@users.noreply.github.com>
2021-11-09 16:02:11 -0500

    Cargue video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -12,8 +12,8 @@
     
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/NpsMouEeSFM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Atención al Cliente en el Retail: introducción
     
 
 </template>
