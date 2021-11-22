7de0abdd4c7aa026570eecd5d75b2bb0cd9bfc9c
elrilacho <80546125+elrilacho@users.noreply.github.com>
2021-11-22 16:44:40 -0500

    Ajustes video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -12,7 +12,7 @@
     
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/NpsMouEeSFM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/c1LwAMyN3Xw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       figcaption Atención al Cliente en el Retail: introducción
     
 
